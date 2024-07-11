32fc7341a6c0471cbf2cec5f1fe8e5b25c902a66
Riccardo Genova <r.genova@bitrocket.dev>
2024-07-11 09:29:50 +0200

    update url and port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 
 const app = express();
 const server = express.json();
+const port = process.env.PORT || 3000;
+const baseURL = process.env.BASE_URL || "http://localhost";
 
 app.use(server);
 
@@ -9,6 +11,6 @@ app.get("/", function (req: Request, res: Response) {
   return res.status(200).send("Server is running...");
 });
 
-app.listen(3000, function () {
-  console.log(`Server is running on http://localhost:3000`);
+app.listen(port, function () {
+  console.log(`Server is running on ${baseURL}:${port}`);
 });
